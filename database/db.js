6de367aed55322d83152888f91941ec08bf7ecf3
Andrey <kenkami1995@gmail.com>
2021-08-06 00:51:11 +0900

    mongo cluster

--- database/db.js
+++ database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/pikselparty'
+    db: 'mongodb+srv://Piksel:PikselParty@cluster0.43kfq.mongodb.net/pikselparty?retryWrites=true&w=majority'
 };
