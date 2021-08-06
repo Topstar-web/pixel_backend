5016d0ecadca28933a24cc6c3e6aac144fb17098
Andrey <kenkami1995@gmail.com>
2021-08-06 23:16:13 +0900

    token

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,17 +15,16 @@ router.route('/check').post((req,res,next) => {
         return res.status(401).json({ message: 'not authenticated' });
     };
     const token = authHeader.split(' ')[1];
-    let decodedToken; 
+    
     try {
         decodedToken = jwt.verify(token, 'secret');
-        console.log(decodedToken);
     } catch (err) {
         return res.status(500).json({ message: err.message || 'could not decode the token' });
     };
     if (!decodedToken) {
         res.status(401).json({ message: 'unauthorized' });
     } else {
-        res.status(200).json({ message: 'here is your resource' });
+        res.status(200).json({ message: 'here is your resource' , "token":decodedToken});
     };
 });
 
